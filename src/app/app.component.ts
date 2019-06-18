1b0d7aa4d60645f0b15039449c47f8df32ef4b33
Tran Son Hoang <hayden.tran.d@gmail.com>
2019-06-18 11:22:50 +0800

    add control at app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FormGroup, FormArray, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  controls: FormArray;
   entities = [
     { id: 1, name: 'Netanel Basal', isAdmin: true },
     { id: 2, name: 'John Due', isAdmin: false },
   ]
+
+  ngOnInit() {
+    const toGroups = this.entities.map( entity => {
+      return new FormGroup({
+        name: new FormControl(entity.name, Validators.required),
+        isAdmin: new FormControl(entity.isAdmin)
+      });
+    });
+    this.controls = new FormArray(toGroups);
+  }
+
+  getControl(index:number, field:string) {
+    return this.controls.at(index).get(field);
+  }
 }
